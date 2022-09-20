ddc8f6248a3ff063da1bbe132d70c6ad2f437150
sgaragagghu <sgaragagghu@gmail.com>
2022-09-20 17:11:07 +0200

    adding create, show, authorize-ssh

---
+++ authorize-ssh.sh
@@ -0,0 +1,1 @@
+aws ec2 authorize-security-group-ingress --group-id [SCRIVI IL SECURITY GROUP ID QUI SENZA PARENTESI] --protocol tcp --port 22 --cidr 0.0.0.0/0
---
+++ create-security-group.sh
@@ -0,0 +1,1 @@
+aws ec2 create-security-group --group-name SDCC-MAIN-EC2 --description "security group that will be used in the main ec2 server" --vpc-id [REPLACE WITH THE VPC ID]
